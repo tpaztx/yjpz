68fe62dc86495d8b900246f2f40a57feaf276595
tpaztx <1150560673@qq.com>
2020-12-11 18:29:03 +0800

    已下架品牌

--- application/api/controller/Store.php
+++ application/api/controller/Store.php
@@ -94,7 +94,7 @@ class Store extends Api
         $downIdArray = $storeDown->getDownId($store['id']);
         $vph = new Wph();
         $list = $vph->brandList('101101', $pageIndex, $pageSize);
-        dump($list['brandList']);exit;
+        dump($list);exit;
         if(!empty($list)){
             $array = [];
             foreach ($list['brandList'] as $k=>$item){
