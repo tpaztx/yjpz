9a97d75a981f79da9790151dee01da7213fa78cd
tpaztx <1150560673@qq.com>
2020-12-11 16:51:52 +0800

    上架测试

--- application/api/controller/Store.php
+++ application/api/controller/Store.php
@@ -57,7 +57,7 @@ class Store extends Api
      */
     public function upBrand()
     {
-        $user = $this->auth->id();
+        $user = $this->auth->getUser();
         dump($user);exit;
         $store = StoreM::getStore($user['id']);
         //获取小店已下架品牌id
