469fff91d35d8cb0bfd073301539da33faa8ee5c
673274466 <673274466@qq.com>
2020-12-15 15:46:21 +0800

    test

--- application/api/controller/Wph.php
+++ application/api/controller/Wph.php
@@ -162,7 +162,7 @@ class Wph extends Api
         $result = $this->brandList($page, $pageSize);
         try {
             foreach ($result as $k => $v) {
-                $goods = GoodsList::where('adId', $v['adId'])->field('goodImage')->select();
+                $goods = GoodsList::where('adId', $v['adId'])->field('goodImage')->limit(5)->select();
                 $result[$k]['goods'] = $goods;
                 $result[$k]['godosTotal'] = count($goods);
             }
