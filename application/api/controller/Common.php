c68fabe2affd00a039ac990b9a7bb7824f6dc37a
673274466 <673274466@qq.com>
2020-12-16 12:41:29 +0800

    test

--- application/api/controller/Common.php
+++ application/api/controller/Common.php
@@ -276,6 +276,7 @@ class Common extends Api
                             $goods_info['adId'] = $v['adId'];
                             $goods_info['goodId'] = $val['goodId'];
                             $goods_info['goodImage'] = $val['goodImage'];
+                            $goods_info['goodName'] = $val['goodName'];
                             $goods_info['logo'] = $val['logo'];
                             $goods_info['sn'] = $val['sn'];
                             $goods_info['vipshopPrice'] = $val['sizes'][0]['vipshopPrice'];
