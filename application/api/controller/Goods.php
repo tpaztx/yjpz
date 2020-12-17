132b2717e67dd91640d7226466bd15524e6cc098
673274466 <673274466@qq.com>
2020-12-17 17:09:30 +0800

    test

--- application/api/controller/Goods.php
+++ application/api/controller/Goods.php
@@ -27,7 +27,7 @@ class Goods extends Api
         $goodFullId = $this->request->request('goodFullId')?:0;
         if ($goodFullId) {
             $wph = new Wph;
-            $goods = Wph::goodsDetailWph('101101', $goodFullId);
+            $goods = $wph->goodsDetailWph('101101', $goodFullId);
             if ($goods && !empty($goods)) {
                 $this->success('ok',$goods);
                 // $result = $goods
