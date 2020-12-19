3e7f91894bc1ca3bed8f9c7554643f268ff0302d
谭鹏 <1150560673@qq.com>
2020-12-19 18:20:03 +0800

    上下架品牌总数

--- application/api/controller/Store.php
+++ application/api/controller/Store.php
@@ -90,7 +90,7 @@ class Store extends Api
                     $array[] = $item;
                 }
             }
-
+            $list = $array;
             $this->success('请求成功！',$list);
         }
         $this->error('无数据！');
