6074163efcb8cdee6bc8f7fa1de47cd6a04f59e2
谭鹏 <1150560673@qq.com>
2020-12-23 18:31:08 +0800

    订单模型创建

---
+++ application/common/model/OrderGood.php
@@ -0,0 +1,20 @@
+<?php
+
+
+namespace app\common\model;
+
+
+class OrderGood
+{
+// 表名
+    protected $name = 'order_good';
+    // 开启自动写入时间戳字段
+    protected $autoWriteTimestamp = 'int';
+    // 定义时间戳字段名
+    protected $createTime = false;
+    protected $updateTime = false;
+    // 追加属性
+    protected $append = [
+
+    ];
+}
