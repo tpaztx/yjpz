deb21bbdd3f70a0b896fe3aff9711bec8885cf7f
673274466 <673274466@qq.com>
2020-12-23 10:04:20 +0800

    sun

--- application/api/controller/Goods.php
+++ application/api/controller/Goods.php
@@ -65,7 +65,7 @@ class Goods extends Api
             foreach ($brand_result as $key => $val)
             {
                 $brand_result[$key]['endTime'] = time2string(strtotime($val['sellTimeTo']) - time());
-                $goods = GoodsList::where('adId', $val['adId'])->field('goodImage,goodId,goodName,sn,vipshopPrice,marketPrice,isMp,commission,color,material,sizes_text,goodBigImage,suggestAddPrice,suggestPrice')->limit(($page - 1)*$pageSize, $pageSize)->select();
+                $goods = GoodsList::where('adId', $val['adId'])->field('goodImage,goodId,goodFullId,goodName,sn,vipshopPrice,marketPrice,isMp,commission,color,material,sizes_text,goodBigImage,suggestAddPrice,suggestPrice')->limit(($page - 1)*$pageSize, $pageSize)->select();
                 foreach ($goods as $k => $v) {
                     $goods[$k]['goodBigImage'] = unserialize($v->goodBigImage);
                 }
