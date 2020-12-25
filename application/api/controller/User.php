76e32544253762feae96fb8b2bcd336f8898d345
谭鹏 <1150560673@qq.com>
2020-12-25 10:11:16 +0800

    微信登录

--- application/api/controller/User.php
+++ application/api/controller/User.php
@@ -403,6 +403,7 @@ class User extends Api
             $data ['province'] = $result ['province'];
             $data ['avatar'] = $result ['headimgurl'];
             $data ['country'] = $result ['country'];
+            $data ['trade_code'] = Random::alnum();
             $data ['type'] = $type;
             $row = \app\admin\model\User::where(['openid' => $result['openid'], 'type' => $type])->find();
             if ($row) {
@@ -423,6 +424,7 @@ class User extends Api
                     $user ['country'] = $result ['country'];
                     $user ['jointime'] = time();
                     $user ['type'] = $type;
+                    $user ['trade_code'] = Random::alnum();
                     $res = $user->save();
                     if (!$res) {
                         $user->delete();
