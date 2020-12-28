239734588514b191e03de3600bd48149c2c895ba
673274466 <673274466@qq.com>
2020-12-28 17:14:16 +0800

    sun

--- application/common/library/Auth.php
+++ application/common/library/Auth.php
@@ -151,7 +151,7 @@ class Auth
             'password' => $password,
             'email'    => $email,
             'mobile'   => $mobile,
-            'level'    => 1,
+            'level'    => 2,
             'score'    => 0,
             'avatar'   => '',
         ];
