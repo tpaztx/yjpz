9192a9d25b6a6775a430177406e17397ea854583
谭鹏 <1150560673@qq.com>
2020-12-30 18:29:50 +0800

    微信登录

--- application/admin/model/User.php
+++ application/admin/model/User.php
@@ -67,32 +67,32 @@ class User extends Model
         return ['normal' => __('Normal'), 'hidden' => __('Hidden')];
     }
 
-    public function getLogintimeTextAttr($value, $data)
-    {
-        $value = $value ? $value : $data['logintime'];
-        return is_numeric($value) ? date("Y-m-d H:i:s", $value) : $value;
-    }
-
-    public function getJointimeTextAttr($value, $data)
-    {
-        $value = $value ? $value : $data['jointime'];
-        return is_numeric($value) ? date("Y-m-d H:i:s", $value) : $value;
-    }
-
-    protected function setPrevtimeAttr($value)
-    {
-        return $value && !is_numeric($value) ? strtotime($value) : $value;
-    }
-
-    protected function setLogintimeAttr($value)
-    {
-        return $value && !is_numeric($value) ? strtotime($value) : $value;
-    }
-
-    protected function setJointimeAttr($value)
-    {
-        return $value && !is_numeric($value) ? strtotime($value) : $value;
-    }
+//    public function getLogintimeTextAttr($value, $data)
+//    {
+//        $value = $value ? $value : $data['logintime'];
+//        return is_numeric($value) ? date("Y-m-d H:i:s", $value) : $value;
+//    }
+//
+//    public function getJointimeTextAttr($value, $data)
+//    {
+//        $value = $value ? $value : $data['jointime'];
+//        return is_numeric($value) ? date("Y-m-d H:i:s", $value) : $value;
+//    }
+//
+//    protected function setPrevtimeAttr($value)
+//    {
+//        return $value && !is_numeric($value) ? strtotime($value) : $value;
+//    }
+//
+//    protected function setLogintimeAttr($value)
+//    {
+//        return $value && !is_numeric($value) ? strtotime($value) : $value;
+//    }
+//
+//    protected function setJointimeAttr($value)
+//    {
+//        return $value && !is_numeric($value) ? strtotime($value) : $value;
+//    }
 
     protected function setBirthdayAttr($value)
     {
