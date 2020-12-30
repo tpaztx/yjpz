558e23f29c18756d7fc3bf82b7417ce2c6f88083
673274466 <673274466@qq.com>
2020-12-30 19:21:08 +0800

    sun

--- application/api/controller/Goods.php
+++ application/api/controller/Goods.php
@@ -439,10 +439,8 @@ class Goods extends Api
         $result = '';
         if ($brands) {
             foreach ($brands as $k => $v) {
-                //查询对应商品list000
-                BrandList::where('adId', $v['adId'])->value('brandName');
-                echo BrandList::getLastSQL();die;
-                $result[$k]['brandName'] = 0000;
+                //查询对应商品list
+                $result[$k]['brandName'] = BrandList::where('adId', $v->adId)->value('brandName');
                 $result[$k]['sizes'] = ShoppingCarts::where('adId', $v->adId)->select();
             }
         }
