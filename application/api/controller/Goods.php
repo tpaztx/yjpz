3983a13438025fd557268f2bbd62f3ca0fca09a9
673274466 <673274466@qq.com>
2020-12-30 09:51:22 +0800

    sun

--- application/api/controller/Goods.php
+++ application/api/controller/Goods.php
@@ -69,10 +69,11 @@ class Goods extends Api
             foreach ($brand_result as $key => $val)
             {
                 $brand_result[$key]['endTime'] = time2string(strtotime($val['sellTimeTo']) - time());
-                $goods = GoodsList::where('adId', $val['adId'])->field('goodImage,goodId,goodFullId,goodName,sn,isMp,color,material,goodBigImage,sizes_json')->limit(($page - 1)*$pageSize, $pageSize)->select();
+                $goods = GoodsList::where('adId', $val['adId'])->field('goodImage,goodId,goodFullId,goodName,sn,isMp,color,material,goodBigImage,vipshopPrice,marketPrice,commission,suggestAddPrice,suggestAddPrice,sizes_json')->limit(($page - 1)*$pageSize, $pageSize)->select();
                 foreach ($goods as $k => $v) {
                     $goods[$k]['isFavorites'] = \app\common\model\Favorites::where(['user_id'=>$this->auth->id, 'goodId'=>$v->goodId])->find()?true:false;
                     $goods[$k]['goodBigImage'] = unserialize($v->goodBigImage);
+                    $goods[$k]['vipshopPrice'] = $v->vipshopPrice + $v->suggestAddPrice;
                     $goods[$k]['total'] = \app\common\model\OrderGood::where('goodId', $v['goodId'])->count('id');
                 }
                 // if ($total==1) {
