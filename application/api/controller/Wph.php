6b0b6337ae90e91523ed7bf5fca0a4a8bf7dd79d
673274466 <673274466@qq.com>
2021-01-04 09:55:58 +0800

    sun

--- application/api/controller/Wph.php
+++ application/api/controller/Wph.php
@@ -24,7 +24,7 @@ class Wph extends Api
     //如果接口已经设置无需登录,那也就无需鉴权了
     //
     // 无需登录的接口,*表示全部
-    protected $noNeedLogin = ['*'];
+    protected $noNeedLogin = ['getBrandList', 'brandList'];
     // 无需鉴权的接口,*表示全部
     protected $noNeedRight = ['*'];
     
