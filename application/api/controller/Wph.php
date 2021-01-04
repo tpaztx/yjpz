31cd87bc3f63e678ba7200c707572e31151d11eb
673274466 <673274466@qq.com>
2021-01-04 09:57:09 +0800

    sun

--- application/api/controller/Wph.php
+++ application/api/controller/Wph.php
@@ -21,7 +21,7 @@ class Wph extends Api
 {
     //如果$noNeedLogin为空表示所有接口都需要登录才能请求
     //如果$noNeedRight为空表示所有接口都需要验证权限才能请求
-    //如果接口已经设置无需登录,那也就无需鉴权了
+    //如果接口已经设置无需登录,那也就无需鉴权了  
     //
     // 无需登录的接口,*表示全部
     protected $noNeedLogin = ['getBrandList', 'brandList'];
