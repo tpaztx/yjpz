319378f30446efbece474bab12ea23ec0b8f3e44
673274466 <673274466@qq.com>
2021-01-05 12:15:41 +0800

    sun

--- application/api/controller/Goods.php
+++ application/api/controller/Goods.php
@@ -403,10 +403,10 @@ class Goods extends Api
             $data['user_id'] = $this->auth->id;
             $data['goodFullId'] = $goodFullId;
             $data['adId'] = $adId;
-            $data['goodName'] = $param['goodName']?'';
-            $data['color'] = $param['color']?'';
-            $data['material'] = $param['material']?'';
-            $data['goodImage'] = $param['goodImage']?'';
+            $data['goodName'] = $param['goodName']?:'';
+            $data['color'] = $param['color']?:'';
+            $data['material'] = $param['material']?:'';
+            $data['goodImage'] = $param['goodImage']?:'';
             $data['createtime'] = time();
             $result = ShoppingCarts::insert($data);
         }
