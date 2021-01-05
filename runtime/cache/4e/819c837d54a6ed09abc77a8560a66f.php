e145fd4ddcc0a36303efa1a301ec7cd45e4750d7
谭鹏 <1150560673@qq.com>
2021-01-05 18:25:34 +0800

    微信登录

--- runtime/cache/4e/819c837d54a6ed09abc77a8560a66f.php
+++ runtime/cache/4e/819c837d54a6ed09abc77a8560a66f.php
@@ -1,4 +1,4 @@
 <?php
 //000000000000
  exit();?>
-a:1:{s:11:"config_init";a:1:{i:0;s:25:"\addons\nkeditor\Nkeditor";}}
+a:4:{s:11:"config_init";a:2:{i:0;s:25:"\addons\nkeditor\Nkeditor";i:1;s:27:"\addons\qcloudsms\Qcloudsms";}s:8:"sms_send";a:1:{i:0;s:27:"\addons\qcloudsms\Qcloudsms";}s:10:"sms_notice";a:1:{i:0;s:27:"\addons\qcloudsms\Qcloudsms";}s:9:"sms_check";a:1:{i:0;s:27:"\addons\qcloudsms\Qcloudsms";}}
